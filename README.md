9e91883f30283e63467441fa6f1f6e91c4333cef
Alex <(ontolawgy)@\1.com>
2013-03-25 09:49:43 +0300

    Initial versions of xmonad configuration documents

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 xmonad-config
 =============
 
-My XMonad configuration files, scripts, and instructions for Ubuntu (12.04)
+XMonad configuration files, scripts, and instructions for Ubuntu (12.04) to use XMonad as a primary desktop with some of the GNOME conveniences underneath (such as working volume control and easy network connection management) 
+
+Simple steps (assuming you have ubuntu and git installed): 
+
+Step 0. Read all of these instructions before you do anything. If you don't understand what something does, look it up. Don't take my word for whether it is/is not safe or will/will not frag your machine. <soapbox>This should be your standard operating procedure before installing or reconfiguring any software. </soapbox>
+
+Step 1. Clone this repo (or just copy the files) wherever you want and change to that directory. 
+
+Step 2. Copy and paste the following into the terminal from your cloned repo or directory, if you trust what I've posted (nothing bad that I can see, but don't take my word for it):
+
+	sudo apt-get update; sudo apt-get install xmonad-contrib xmobar kupfer xcompmgr synaptic gnome-panel hsetroot scrot
+	sudo mv xmonad-xstart.desktop /usr/share/xsessions/.
+	sudo mv xmonad.start /usr/local/bin/. ; sudo chmod +x /usr/local/bin/xmonad.start
+	mkdir -p ~/.xmonad
+	mv xmonad.hs ~/.xmonad/.
+	mv xmobarrc ~/.xmonad/.
+	mkdir -p ~/bin; mv scs ~/bin/. ; chmod +x ~/bin/scs
+	git clone https://github.com/jgoerzen/ledmon.git
+	cd ledmon; make; mv ledmon ~/bin/ledmon; cd -
+	xmonad --recompile
+
+Step 3. If you're in an X session, Logout of your current session, then select "Xmonad (with GNOME panel)" from your display manager, and login there. Things will take a few seconds (or less) to load once you login.
+
+Step 4. When you login, you will (hopefully) see a GNOME panel at the top of the screen and a blank black screen. Don't panic: you can use "feh" or other programs to set a desktop background; I set mine to black for power saving and to keep things uncluttered; you can change this in your xmonad.start script. Alt+Right click on the panel, select properties, then check "Autohide" and click OK, and you will have what I think is the best of both worlds. You may have to dip in and out of the panel once or twice to get it to hide, but it will hide, and you will see your xmobar (black bar with green and orange lettering).  You may (likely) have to re-start XMonad on every login to get it working properly. It's just a reflex for me at this point (Ctrl+Win+R) and it takes a fraction of a second, so I don't care that much, but perfectionists may find it irritating.
+
+Step 5. Open Kupfer (Win+P then type kupfer -- you may have to restart XMonad with a Ctrl+Win+R to get this to work) and set up a keyboard shortcut for it (click the gear) - pick something that doesn't conflict with XMonad, i.e., something that does not include the Windows key. This will give you a quick way to launch programs via a GUI, often with less typing than the run-or-raise prompt. You can also set up any other GNOME-related keyboard shortcuts you want through the GNOME control panel. 
+
+Step 6. Practice. That's right, you will probably need to practice using XMonad. Look up the default keyboard shortcuts (<http://www.haskell.org/haskellwiki/Xmonad#Keybindings_cheatsheets>), then note where they differ in this xmonad.hs file; or change them back to the defaults if you want (it's just two dashes and a recompile away). Once you get the hang of it (maybe an hour or so; less if you are familiar with vim) you will probably either be hooked or never use XMonad again. 
+
+Step 7. Enjoy (or not). After a year of using XMonad all day, every day for a mix of programming, research, writing, video conferences, and email, I find it excruciating to use any other window manager (much less someone else's computer), particularly if I have to get any work done. 		
+
+Notes: 
+
+1. Accept all recommended dependencies in the apt-get commands (synaptic isn't strictly needed, though; it's just a lot more powerful and reliable than Software Center, in my experience). Warning: GHC is MASSIVE; it's kind of funny to me that it takes a 600 MB+ download to get a window manager running that uses < 7 MB of RAM (I'm reading 6.3MB now with 14 windows open on 6 different workspaces--try that with GNOME), but that's the price you pay... It also lets you play around with Haskell, if you're so inclined. 
+
+2. For security, in all the configuration files--and even the commands above--it is recommended to replace ~ with /home/yourusername. 
+
+3. There are some quirks:
+	-With the default XMonad and GHC provided in Ubuntu 12.04 and these configuration files, you typically need to restart XMonad on every login to get a working "run or raise" dialog
+	-GIMP's main image window doesn't automatically get shifted to the master position. 
+	-Sometimes my mouse will freeze; not sure if that's a hardware or software issue (and if software, which program), but I seem to be able to fix it every time by opening a terminal (Window + Enter, using the shortcuts in this configuration) and typing: 
+	
+		killall gnome-settings-daemon; (gnome-settings-daemon &) 
+
+	then close the window if you don't want to be bothered with the occassional text flashing through it. 
+
+5. I've tried to comment the xmonad.hs file as much as possible where I think that settings or reasons for them aren't necessarily clear. 
---
+++ xmonad.hs
@@ -0,0 +1,208 @@
+{-# OPTIONS_GHC -W -fwarn-unused-imports -fno-warn-missing-signatures #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import XMonad
+import XMonad.Core
+import Data.List
+import Data.Maybe
+import System.Exit
+import System.IO
+
+import XMonad.Actions.CycleWS
+import XMonad.Actions.CopyWindow(copy)
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.GridSelect
+import XMonad.Actions.UpdatePointer()
+import XMonad.Actions.Warp
+import XMonad.Actions.WindowMenu
+import XMonad.Actions.WindowGo
+
+import XMonad.Config.Gnome
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.SetWMName
+
+import XMonad.Layout.Decoration
+import XMonad.Layout.DecorationMadness
+import XMonad.Layout.IndependentScreens
+import XMonad.Layout.MultiColumns
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TabBarDecoration
+import XMonad.Layout.ZoomRow
+
+
+import XMonad.ManageHook
+
+import XMonad.Prompt
+import XMonad.Prompt.Man
+import XMonad.Prompt.RunOrRaise
+import XMonad.Prompt.Shell
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Font
+import XMonad.Util.Run
+import XMonad.Util.Themes
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import Graphics.X11
+--End imports
+
+-- Borders
+--
+--- | Width of the window border in pixels.
+----
+myBorderWidth :: XMonad.Dimension
+myBorderWidth = 1
+--
+---- | Border colors for unfocused and focused windows, respectively.
+myNormalBorderColor  = "black" -- "#dddddd"
+myFocusedBorderColor = "green"  -- "#ff0000" don't use hex, not <24 bit safe
+---
+
+--Main
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc" --for security, it is a good idea to hard-code ~ to /home/username here; ~ is being used for portability only and is NOT recommended for use in a production environment. Same goes for everywhere else you see ~
+    --xmonad $ ewmh $ withUrgencyHook NoUrgencyHook $ gnomeConfig 
+    xmonad $ withUrgencyHook NoUrgencyHook $ gnomeConfig 
+	{ workspaces = myWorkspaces
+	, terminal = myTerminal
+	, borderWidth        = myBorderWidth
+	, normalBorderColor  = myNormalBorderColor
+    	, focusedBorderColor = myFocusedBorderColor
+	--, manageHook = myManageHook <+> manageHook defaultConfig
+	, manageHook = myManageHook <+> manageHook gnomeConfig
+	, layoutHook = avoidStruts $ myLayout
+	, startupHook = setWMName "LG3D" -- required to get some Java GUI applications running properly
+        , logHook = dynamicLogWithPP xmobarPP
+        	{ ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 150
+                , ppCurrent = xmobarColor "green" "" . wrap "[*" "*]"
+		, ppVisible = xmobarColor "green" "" . wrap "[" "]"
+     		, ppHidden = xmobarColor "yellow" "#838383" . wrap "" ""
+		, ppUrgent = xmobarColor "red" "" . wrap "-!!" "!!-"
+		-- tried the following, but it doesn't work; problem is that Skype does not respect the X11 Urgent flag; however, running the GNOME panel with unified notification applet will allow Skype to show popup notifications
+		--, ppUrgent = xmobarColor "black" "red" . xmobarStrip
+		-- uncomment the following line to show ids for workspaces with no windows
+		--, ppHiddenNoWindows = xmobarColor "grey" "" . wrap "| " " |"
+		, ppLayout = const "" -- disables layout info on xmobar; comment to show layout names
+                }
+	, modMask = XMonad.mod4Mask --Rebind Mod to Windows key
+	} `additionalKeysP` myKeys 
+
+-- <Variables>
+
+-- <Workspaces>
+
+myWorkspaces = ["`", "1-t", "2-e", "3-§", "4-f", "5-o", "6", "7-$", "8-web", "9-vb", "0", "-", "gimp"] 
+--Why 13 workspaces? ` = monitoring apps, both console and GUI; 1 = Terminal; 2 = Email; 3 = Skype and google chat; 4 - file manager; 5 = office applications 6 = spare, often used for reading extra PDFs while writing about them on workspace 5 with a PDF and a LibreOffice Writer document open next to it; 7 = spreadsheets; 8 = web browsing; 9 = VirtualBox; 0 = spare; - = spare; gimp = GIMP, with a slightly tweaked custom layout that works well for me (just raise the main image window to master)
+--
+--myWorkspaces = ["1-t", "2-e", "3-§", "4-f", "5-o", "6", "7-$", "8-web", "9-vb"] -- default 9 workspaces with quick notes about what they're for
+
+-- <Default applications>
+
+myBar = "xmobar"
+myTerminal = "gnome-terminal"
+
+-- <Appearance>
+
+xftFont = "xft:Ubuntu:weight=bold:size=10"
+
+myTheme = defaultTheme {
+	  fontName = xftFont
+		}
+
+myXPConfig = defaultXPConfig {
+	font = xftFont
+}
+
+-- <GridSelect>
+myGSConfig colorizer = (buildDefaultGSConfig colorizer)
+   { gs_cellheight   = 50
+   , gs_cellwidth    = 350
+   , gs_cellpadding  = 10
+   , gs_font = xftFont
+   }
+
+-- warpToCenter brings the pointer to the center of the grid for gridSelect
+warpToCenter = gets (W.screen . W.current . windowset) >>= \x -> warpToScreen x  0.5 0.5
+
+-- <Layouts>
+
+---Tabbed - shows at a glance which apps/windows are open on a given workspace
+
+myLayout = 	onWorkspace "9-vb" simpleTabbed $ 
+		onWorkspace "gimp" gimpTabbedMultiColumn $
+		tabbedMultiColumn ||| Mirror tiled ||| simpleTabbed ||| Full
+	where
+		fontName = xftFont
+		tabbedMultiColumn = simpleTabBar baseColumns
+		gimpTabbedMultiColumn = simpleTabBar gimpColumns
+		tabbedZoomRow = simpleTabBar zoomRow		
+		baseColumns = multiCol [1] 0 0.03 0.56 -- use Mod-, or Mod-. to change the number of windows per column; Mod-S-Space resets to default
+		gimpColumns = multiCol [1] 1 0.03 0.56
+		tiled = Tall nmaster delta ratio
+		nmaster = 1
+		ratio = 0.56 --roughly 1 / sqrt(5)
+		delta = 3/100
+
+-- <Automatic application and window actions>
+
+myManageHook = composeAll . concat $
+ 	[ [isDialog --> doFloat] -- This allows you to move things like modal dialogs, file open/save windows so you can peek underneath if you need to
+	, [className =? "Firefox" --> doShift "8-web"
+ 	, appName =? "google-chrome" --> doShift "8-web"
+	, className =? "Xmessage" --> doCenterFloat
+	, className =? "Do"	--> doCenterFloat
+	, className =? "Kupfer"	--> doCenterFloat
+	, className =? "Gimp-2.6" <&&> stringProperty "WM_WINDOW_ROLE" =? "gimp-image-window" --> doF W.shiftMaster -- doesn't work
+	, className =? "Gimp-2.6" <&&> stringProperty "WM_WINDOW_ROLE(STRING)" =? "gimp-image-window" --> doF W.shiftMaster --doesn't work
+	, className =? "Gimp-2.6"	--> doShift "gimp" --works
+	, className =? "Skype"	--> doShift "3-§"
+	, manageDocks 
+		]
+    ]
+
+-- <Keys>
+
+myKeys :: [(String, X ())] -- this is needed to get functioning workspace switching keys for extended workspaces using the keys set below
+myKeys =
+	[ ("<Print>", spawn "sleep 0.2; sh ~/bin/scs &")  --This is a small shell script to use scrot to take screenshots into /run/shm named with the date and time of the picture. For security, it is a good idea to hard-code ~ to /home/username here; ~ is being used for portability only and is NOT recommended for use in a production environment
+	, ("M-S-q", spawn "xmessage 'Use Ctrl-Mod-Shift-q to exit.'") --This is the default keybinding to kill XMonad, and it is easy to hit by accident, with sometimes disastrous results. I have unmapped it here to help prevent unintentional exits. XMonad is a bit abrupt on exiting and will dump you to the display manager without asking you to save anything.
+	, ("M-C-S-q", io (exitWith ExitSuccess)) --In more than a year of using XMonad as my only desktop, I have never exited by accident with the key combination set to this; just try hitting 4 keys by accident.
+	, ("M-q", spawn "xmonad --recompile; xmessage 'XMonad recompiled'") --Unmaps M-q from recompile and restart to prevent XMo from restarting after a failed recompile
+	, ("M-C-r", spawn "xmonad --restart")
+	, ("M-a", warpToCenter >> goToSelected (myGSConfig defaultColorizer) )
+	, ("M-x", windowMenu)
+	, ("M-<L>", prevWS) --change view to previous workspace
+	, ("M-S-<L>", shiftToPrev) --Move client to previous workspace
+	, ("M-<R>", nextWS) --change view to next workspace
+	, ("M-S-<R>", shiftToNext) --Move client to next workspace
+	, ("M-p", runOrRaisePrompt myXPConfig 
+			{position = Top
+			})
+	, ("M-z", warpToWindow (0.1) (0.1)) -- Move pointer to currently focused window, 10% in from the upper left corner
+	, ("M-u", focusUrgent)
+	, ("M-b", sendMessage ToggleStruts)
+	, ("M-<Return>", spawn myTerminal) -- change from default
+	, ("M-S-<Return>", windows W.shiftMaster) --per xmonad-contrib-0.10 README to move current window to master in current workspace to be more consistent with workspace shifting commands
+	]
+	++
+--To use extra workspaces as laid out in this configuration, use this codeblock; [key] needs to be in brackets, workspaceKeys needs to be out of them; didn't work the other way around
+ 	[ (otherModMasks ++ "M-" ++ [key], action tag)
+        	| (tag, key)  <- zip myWorkspaces workspaceKeys
+	        , (otherModMasks, action) <- [ ("", windows . W.view) -- lets you move focus to the workspace on the other monitor without swapping them
+        				     ,  ("S-", windows . W.shift)]
+	]
+	++
+ 	[ (otherModMasks ++ "M-C-" ++ [key], action tag)
+        	| (tag, key)  <- zip myWorkspaces workspaceKeys
+	        , (otherModMasks, action) <- [ ("", windows . W.greedyView) -- instead of switching focus to the visible workspace on another monitor, this swaps workspaces between monitors, which is the default behavior, and which I found maddening when using VirtualBox with Windows on a large external monitor, and would frequently unintentionally shift it to my laptop, which would resize all the windows inside.
+        				     ,  ("S-", windows . W.shift)] --the intention is something like a "greedy shift", but it doesn't seem to work for me; just M-S-#, then M-C-#; again, something for a perfectionist to try to fix
+	]
+	where workspaceKeys = "`1234567890-=" --these are the literal keys; = goes to the gimp workspace; change to "123456789" to get default keybindings back, but make sure you only have 9 workspaces declared or you won't have a very easy way to get to them all 
